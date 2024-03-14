e9ad2f8e498226113b1ac506e809ced140eaa2a5
ongyx <dai_guru@yahoo.com>
2024-03-13 17:22:32 -0700

    Add Text input and image output element

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,19 +2,45 @@ import { useState } from "react";
 
 const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [textToEncode, setTextToEncode] = useState();
+  const [encodedImgUrl, setEncodedImgUrl] = useState(null);
 
-  const handleFileUpload = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const handleEncode = (e) => {
+    if (!selectedFile) {
+      alert(`No File selected. Please select an image.`);
+      return;
+    }
+    const validImageTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (!validImageTypes.includes(selectedFile.type)) {
+      alert(`Invalid file type. Please select an image (JPEG, PNG, or GIF).`);
+      return;
+    }
+    alert(`Hiding the message '${textToEncode}' in ${selectedFile.name}`);
+
+    // Display the encoded image
+    const encodedImg = selectedFile;
+    const reader = new FileReader();
+    reader.readAsDataURL(encodedImg);
+    // Update image URL state
+    reader.onloadend = () => setEncodedImgUrl(reader.result);
   };
 
   return (
     <div>
-      <input type="file" id="fileInput" onChange={handleFileUpload} />
-      {selectedFile && (
+      <div>
         <div>
-          <p>Selected file: {selectedFile.name}</p>
+          <label>Image: </label>
+          <input type="file" id="fileInput" onChange={(e) => setSelectedFile(e.target.files[0])} />
         </div>
-      )}
+        <div>
+          <label>Text to encode: </label>
+          <input type="text" id="textInput" value={textToEncode} onChange={(e) => setTextToEncode(e.target.value)} />
+        </div>
+      </div>
+      <div>
+        <button onClick={handleEncode}>Encode</button>
+      </div>
+      <div>{encodedImgUrl && <img src={encodedImgUrl} alt="Encoded Image" />}</div>
     </div>
   );
 };
